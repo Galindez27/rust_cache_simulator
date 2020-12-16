2f67d3eff84022226e8da5ab019e91dfe6a1a7c1
Jon Galindez <galindez@bu.edu>
2020-12-16 01:30:27 -0500

    Proof of concept that a vec of linked lists is possible.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::collections::linked_list; // starting point
+
 fn main() {
-    println!("Hello, world!");
+    let x = 10;
+    let mut y: Vec<linked_list::LinkedList<i32>> = Vec::with_capacity(x);
+    for _z in 0..9 {
+        y.push(linked_list::LinkedList::new());
+        let z = &mut y[_z];
+        z.push_back(69);
+    }
+    
+    let j = &y[4];
+    assert!(j.contains(&69), "Does not contain 69!"); //passes
+    assert!(j.contains(&60), "Does not contain 60!"); //panics
 }
